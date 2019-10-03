51b3676ecc574b19516c775691c4ef89da05b246
DNAgb <43895611+DNAgb@users.noreply.github.com>
2019-10-03 16:44:47 -0700

    drawing machine update

--- sketch.js
+++ sketch.js
@@ -1,9 +1,28 @@
 function setup() {
   createCanvas(600, 600);
-  background(260);
+  background(220, 60, 70);
+
+  strokeWeight(4);
+
 }
 
 function draw() {
 
-  
+  if (mouseIsPressed) {
+    stroke(map(mouseX, 0, 600, 0, 210, true))
+    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+  }
+}
+
+function keyTyped() {
+  console.log(`key ${key} is being typed`)
+  console.log("key" + key + "is being typed")
+  if (key === 's') {
+    //save this image
+    saveCanvas('fileName', 'png')
+
+  }
+  return false;
+
 }

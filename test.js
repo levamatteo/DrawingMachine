1eae5115d089ac760b659a638c0dc65c9d5e7e96
DNAgb <43895611+DNAgb@users.noreply.github.com>
2019-10-08 15:25:51 -0700

    class ex uodate

---
+++ test.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(600, 600);
+  background(220, 60, 70);
+
+  strokeWeight(4);
+
+}
+
+function mousePressed() {
+  array = [];
+  backgroundColor = 255
+
+
+}
+
+function drawGrid() {
+
+  for (let i = 0; i < 3; i++) {
+    Ellipse(i * 100, 50, 50, 50);
+
+    for (let j = 0; j, 3; j++) {
+      ellipse
+    }
+  }
+}

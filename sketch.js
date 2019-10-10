c43b9f1d929df15af0572ffddb999c0a6c8a4ab4
DNAgb <43895611+DNAgb@users.noreply.github.com>
2019-10-10 14:31:37 -0700

    Noise update

--- sketch.js
+++ sketch.js
@@ -1,13 +1,19 @@
+let noiseOffset = 0.0
+let strokeWidth = 4
+
 function setup() {
   createCanvas(600, 600);
   background(220, 60, 70);
 
-  strokeWeight(4);
+
 
 }
 
 function draw() {
+  strokeWeight(strokeWidth);
 
+  noiseOffset += 0.01
+  strokeWidth = noise(noiseOffset) * 100  
   if (mouseIsPressed) {
     stroke(map(mouseX, 0, 600, 0, 210, true))
     line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
@@ -16,11 +22,19 @@ function draw() {
 }
 
 function keyTyped() {
-  console.log(`key ${key} is being typed`)
-  console.log("key" + key + "is being typed")
+  // console.log(`key ${key} is being typed`)
+  // console.log("key" + key + "is being typed")
   if (key === 's') {
     //save this image
     saveCanvas('fileName', 'png')
+
+  } else if (key === 'd') {
+    //display image
+
+    for (let i = 0; i < array.length - 1; i++) {
+      // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1], )
+      curvedVertex(array[i][0], array[i][1])
+    }
 
   }
   return false;

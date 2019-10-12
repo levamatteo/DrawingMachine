f1bce15cf673bd2546281e5092b3d7c1127cf69d
DNAgb <43895611+DNAgb@users.noreply.github.com>
2019-10-12 14:25:12 -0700

    full scree n update

--- sketch.js
+++ sketch.js
@@ -1,40 +1,63 @@
-let noiseOffset = 0.0
+// let noiseOffset = 0.0
 let strokeWidth = 4
-let bg;
+let img;
+let array = [];
+function preload() {
+  img = loadImage('images/stra.jpeg');
+                  }
 function setup() {
-  createCanvas(600, 600);
-  background(220, 60, 70);
-bg = loadImage('images/Stars1.jpg');
-
+  createCanvas(windowWidth, windowHeight);
+background(img);
+// image(img, 0, 0, 600, 600);
+noFill();
 
 }
 
 function draw() {
-background(bg);
+
+
   strokeWeight(strokeWidth);
 
-  noiseOffset += 0.01
-  strokeWidth = noise(noiseOffset) * 100
+  // noiseOffset += 0.01
+  // strokeWidth = noise(noiseOffset) * 100
+
   if (mouseIsPressed) {
-    stroke(map(mouseX, 0, 600, 0, 210, true))
-    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
-    line(mouseX, mouseY, pmouseX, pmouseY);
+    //stroke(map(mouseX, 0, 600, 0, 210, true))
+    // line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+stroke(245, 245, 245, 100);
+// line(mouseX, mouseY, pmouseX, pmouseY);
+array.push([mouseX, mouseY]);
   }
 }
 
 function keyTyped() {
-  // console.log(`key ${key} is being typed`)
-  // console.log("key" + key + "is being typed")
+
   if (key === 's') {
     //save this image
     saveCanvas('fileName', 'png')
 
   } else if (key === 'd') {
     //display image
+strokeWeight(3);
+stroke(255, 0, 0, 50)
+    beginShape();
+        for (let i = 0; i < array.length - 1; i++) {
+
+          // line(array[0][0], array[0][1], array[1][0], array[1][1]);
+        // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+          curvedVertex(array[i][0], array[i][1])
+          endShape();
+        }
+          strokeWeight(1);
+          stroke(255);
+beginShape();
 
     for (let i = 0; i < array.length - 1; i++) {
-      // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1], )
+
+      // line(array[0][0], array[0][1], array[1][0], array[1][1]);
+    // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
       curvedVertex(array[i][0], array[i][1])
+      endShape();
     }
 
   }
